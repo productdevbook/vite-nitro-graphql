0c2d79f083de79fdf07331d68b0aa89f1ea0a10a
productdevbook <hi@productdevbook.com>
2025-11-03 22:48:28 +0300

    fix: add missing comma in Vite configuration for GraphQL framework

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
     preset: "standard",
     modules: ["nitro-graphql"],
     graphql: {
-      framework: 'graphql-yoga'
-    }
+      framework: 'graphql-yoga',
+    },
   },
 });
