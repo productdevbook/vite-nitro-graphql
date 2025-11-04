bca40138932a3a6cb3ebad817995bdb92325f08c
productdevbook <hi@productdevbook.com>
2025-11-04 12:11:53 +0300

    feat: update nitro-graphql dependency to version 2.0.0-beta.13 and adjust Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,17 +4,15 @@ import { graphql } from 'nitro-graphql/vite'
 
 export default defineConfig({
   plugins: [
-    graphql(),
-    nitro()
-  ],
-  nitro: {
-    preset: "standard",
-    modules: ["nitro-graphql"],
-    graphql: {
+    graphql({
       framework: 'graphql-yoga',
       paths: {
         serverGraphql: 'routes/graphql',
       },
-    },
+    }),
+    nitro()
+  ],
+  nitro: {
+    preset: "standard",
   },
 });
